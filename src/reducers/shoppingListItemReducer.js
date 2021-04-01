b770548134d301e9115f5d900287175b7c5fe623
github username <scott.gloyna01@gmail.com>
2021-04-01 11:30:23 -0500

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,7 +1,6 @@
-export default function shoppingListItemReducer(state = {
-  items: []
-}, action) {
+export default function shoppingListItemReducer( state = { items: [] }, action ) {
   console.log(action);
+  
   switch(action.type) {
 
     case 'INCREASE_COUNT':
